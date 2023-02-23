5e0678b27be6413b4175cf3c1e0f858d4f548eac
YuYuan <moshou5571228@gmail.com>
2023-02-23 21:41:42 +0800

    create config.js to set up

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const mongoose = require('mongoose');
+const {MONGO_USER,MONGO_IP, MONGO_PASSWORD, MONGO_PORT } = require("./config/config");
 
 const app = express();
 
-mongoose.connect("mongodb://root:example@mongo:27017/?authSource=admin")
+mongoose.connect(`mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`)
 .then(() => console.log("succesfully connected to DB"))
 .catch((e) => console.log(e));
 
